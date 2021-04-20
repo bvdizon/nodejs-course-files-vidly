70862d1ae09ce4052b9ce8775a22af10869bf4b7
briandizon <bvdizon@gmail.com>
2021-04-21 02:45:31 +0800

    Genre - model and route created

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const genres = require('./routes/genres');
 const app = express();
 
 mongoose
-  .connect('mongodb://localhost/bvdvidly')
+  .connect('mongodb://localhost/bvdvidly', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log('Connected to MongoDB...'))
   .catch((err) => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
+app.use('/api/genres', genres);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
