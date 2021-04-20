275ed0caf76214cb78736ca6e889a340260addc0
briandizon <bvdizon@gmail.com>
2021-04-21 03:06:17 +0800

    Movie - model and routes created.

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 const express = require('express');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
 const app = express();
 
 mongoose
   .connect('mongodb://localhost/bvdvidly', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
   })
   .then(() => console.log('Connected to MongoDB...'))
   .catch((err) => console.error('Could not connect to MongoDB...'));
@@ -15,6 +17,7 @@ mongoose
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
