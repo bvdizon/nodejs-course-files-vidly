eeb035a436f514d963c7027f84225e0462fe06f5
briandizon <bvdizon@gmail.com>
2021-04-21 02:35:35 +0800

    First commit. Server and DB setup.

---
+++ index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+mongoose
+  .connect('mongodb://localhost/bvdvidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch((err) => console.error('Could not connect to MongoDB...'));
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
