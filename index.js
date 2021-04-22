1f462b6723450d32eb3ee02d42bd40decd724542
briandizon <bvdizon@gmail.com>
2021-04-23 01:22:57 +0800

    Users = model and routes created.

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+const config = require('config');
 const mongoose = require('mongoose');
 const express = require('express');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
 const app = express();
 
+if (!config.get('jwtPrivateKey')) {
+  console.error(`FATAL Error: jwtPrivateKey is not defined or set.`);
+  process.exit(1);
+}
 mongoose
   .connect('mongodb://localhost/bvdvidly', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false,
   })
   .then(() => console.log('Connected to MongoDB...'))
@@ -20,6 +27,7 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ models/user.js
@@ -0,0 +1,51 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 1024,
+  },
+  isAdmin: Boolean,
+});
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    config.get('jwtPrivateKey')
+  );
+  return token;
+};
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(50).required().email(),
+    password: Joi.string().min(5).max(1024).required(),
+    isAdmin: Joi.boolean(),
+  });
+
+  return schema.validate(user);
+}
+
+exports.User = User;
+exports.validate = validateUser;
